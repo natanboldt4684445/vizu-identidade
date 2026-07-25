ffc9c4e0489b881e141cbcbf43ffcf793ddc89b4
Natan Boldt <natanboldt@gmail.com>
2026-07-25 20:58:35 -0300

    Serviço criado e configurado

---
+++ src/main/java/com/vizu/identidade/IdentidadeApplication.java
@@ -0,0 +1,18 @@
+package com.vizu.identidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
+public class IdentidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vizu/identidade/IdentidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vizu.identidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
